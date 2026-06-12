125daab3949281c34f2bd201cd2e44339bbb4437
nada essam <nadaessam99e@gmail.com>
2026-06-12 16:28:56 +0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,73 @@
+#####################################
+# VPC
+#####################################
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+
+  name = "my-eks-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  tags = {
+    "kubernetes.io/cluster/my-eks-cluster" = "shared"
+  }
+}
+
+#####################################
+# EKS CLUSTER
+#####################################
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20"
+
+  cluster_name    = "my-eks-cluster"
+  cluster_version = "1.29"
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  #####################################
+  # API ACCESS (IMPORTANT)
+  #####################################
+  cluster_endpoint_public_access  = true
+  cluster_endpoint_private_access = true
+
+  #####################################
+  # IAM ACCESS FOR USER
+  #####################################
+  access_entries = {
+    admin = {
+      principal_arn = "arn:aws:iam::114223852322:user/nada"
+
+      policy_associations = {
+        admin = {
+          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+
+          access_scope = {
+            type = "cluster"
+          }
+        }
+      }
+    }
+  }
+
+  #####################################
+  # MANAGED NODE GROUPS
+  #####################################
+  eks_managed_node_groups = {
+    default = {
+      instance_types = ["t3.medium"]
+
+      min_size     = 1
+      max_size     = 3
+      desired_size = 2
+    }
+  }
+}
